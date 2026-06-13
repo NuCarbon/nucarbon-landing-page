884f11f3df9b9ca50ef854628a6fdccfd222ba77
Igor <igor.mandello@origin-games.com>
2026-06-13 11:26:14 +0100

    chore: starts working on SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import { defineConfig, fontProviders } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://nucarbon.netlify.app',
+    site: 'https://nucarbon.com.br',
     fonts: [{
         provider: fontProviders.local(),
         name: "Kollektif",
@@ -15,5 +16,6 @@ export default defineConfig({
             style: 'normal'
           }]
         }
-      }]
+      }],
+    integrations: [sitemap()],
 });
